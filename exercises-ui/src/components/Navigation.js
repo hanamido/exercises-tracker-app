c9ad085cccbeff5f82c73940547374f43128ff3b
hanamilktea <dohan@oregonstate.edu>
2022-08-27 03:37:21 -0700

    updated ui files

---
+++ exercises-ui/src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+
+function Navigation () {
+const navigate = useNavigate();
+
+    return (
+        <nav className="App">
+            <button><Link to="/" onClick={() => navigate('/')}>Home</Link></button>
+            <button><Link to="/add-exercise">Add Entry </Link></button>
+        </nav>
+    )
+}; 
+
+export default Navigation;
